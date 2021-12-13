71c0f288979af40fdaa39f519b6f4080d95f38a6
margarita-v <margarita-v@users.noreply.github.com>
2021-12-13 11:29:17 +0000

    Code formatting [skip ci] [version]

--- accompanist/src/main/kotlin/ru/surfstudio/compose/accompanist/extensions/Modifier.kt
+++ accompanist/src/main/kotlin/ru/surfstudio/compose/accompanist/extensions/Modifier.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Vitaliy Zarubin
+ * Copyright 2021 Surf LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package ru.surfstudio.compose.accompanist.extensions
 
 import androidx.compose.ui.Modifier
