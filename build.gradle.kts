b79eb1f90e57e05f6a9d321bcfaefd3ce97a14b8
Keygenqt <keygenqt@gmail.com>
2022-10-12 19:35:49 +0300

    Update android versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
-    id("com.jfrog.artifactory")
     id("com.diffplug.spotless")
 }
-
-val composeVersion: String = findProperty("composeVersion") as? String ?: "1.1.0-rc01"
-val accompanistVersion: String = findProperty("accompanistVersion") as? String ?: "0.22.0-rc"
-val pagingComposeVersion: String = findProperty("pagingComposeVersion") as? String ?: "1.0.0-alpha14"
 
 version = "0.0.14"
 group = "com.keygenqt.accompanist"
@@ -20,43 +14,18 @@ spotless {
     }
 }
 
-publishing {
-    publications {
-        register("aar", MavenPublication::class) {
-            groupId = group.toString()
-            artifactId = project.name
-            artifact("$buildDir/outputs/aar/keygenqt-accompanist-$version-debug.aar")
-        }
-    }
-}
-
-artifactory {
-    setContextUrl("https://artifactory.keygenqt.com/artifactory")
-    publish {
-        repository {
-            setRepoKey("open-source")
-            setUsername(findProperty("arusername").toString())
-            setPassword(findProperty("arpassword").toString())
-        }
-        defaults {
-            publications("aar")
-            setPublishArtifacts(true)
-        }
-    }
-}
-
 android {
 
-    compileSdk = 30
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 31
+        targetSdk = 33
         setProperty("archivesBaseName", "keygenqt-accompanist-$version")
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerExtensionVersion = findProperty("composeCompilerVersion").toString()
     }
 
     buildFeatures {
@@ -65,8 +34,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.paging:paging-compose:$pagingComposeVersion")
-    implementation("com.google.accompanist:accompanist-swiperefresh:$accompanistVersion")
+    implementation("androidx.compose.ui:ui:1.1.0-rc01")
+    implementation("androidx.compose.material:material:1.1.0-rc01")
+    implementation("androidx.paging:paging-compose:1.0.0-alpha14")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.22.0-rc")
 }
